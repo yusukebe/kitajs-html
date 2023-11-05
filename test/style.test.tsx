2caf195bba69b19c5f389f32bb1387876095dd99
Nick Saunders <nick@saunde.rs>
2023-11-04 21:55:39 -0700

    fix: `style` prop type. Resolves #60. (#61)
    * revert: fix: react-like object styles (#60)
    
    This reverts commit ea35e41ca641186338d26ca87e12f413b6de0a8f.
    
    * fix: style prop should only accept string lengths

--- test/style.test.tsx
+++ test/style.test.tsx
@@ -25,21 +25,6 @@ describe('Style', () => {
     assert.equal(<div style={style} />, '<div style="color:red;not:defined;"></div>');
   });
 
-  test('Same behavior as react', () => {
-    assert.equal(
-      <div
-        style={{
-          height: false,
-          width: true,
-          maxWidth: 123,
-          maxHeight: 0,
-          minWidth: '12a3'
-        }}
-      ></div>,
-      <div style="max-width:123px;max-height:0;min-width:12a3;"></div>
-    );
-  });
-
   test('Weird values', () => {
     assert.equal(
       styleToString({
@@ -47,22 +32,9 @@ describe('Style', () => {
         b: undefined,
         c: 1,
         d: '2',
-        e: { f: 3 },
-        f: null,
-        g: true,
-        h: false,
-        i: NaN,
-        j: Infinity,
-        k: -Infinity,
-        l: '',
-        m: ' ',
-        n: [],
-        o: [1, 2, 3],
-        p: {},
-        q: { a: 1, b: 2, c: 3 }
+        e: { f: 3 }
       }),
-      // Similar behavior to react
-      'a:0;c:1px;d:2;e:[object Object];i:NaNpx;j:Infinitypx;k:-Infinitypx;l:;m: ;n:;o:1,2,3;p:[object Object];q:[object Object];'
+      'a:0;c:1;d:2;e:[object Object];'
     );
   });
 });

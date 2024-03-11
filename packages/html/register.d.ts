71e951568851a10ee835f12f77f6ebe5d8e98428
Arthur Fiorette <me@arthur.place>
2024-03-11 13:09:00 -0300

    docs: deprecated global Html var

--- packages/html/register.d.ts
+++ packages/html/register.d.ts
@@ -1,5 +1,11 @@
 declare global {
-  /** The html factory namespace. */
+  /**
+   * The html factory namespace.
+   *
+   * @deprecated The `@kitajs/html/register` import has been DEPRECATED and will be
+   *   removed in the next major version. See
+   *   https://github.com/kitajs/html#deprecating-global-register
+   */
   var Html: typeof import('./index');
 }
 

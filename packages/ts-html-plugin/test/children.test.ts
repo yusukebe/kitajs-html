c50d4a76db7b7638dae3ce136f402b855d20cc5b
Arthur Fiorette <me@arthur.place>
2024-04-02 22:57:40 -0300

    test: ensure #145 is working

--- packages/ts-html-plugin/test/children.test.ts
+++ packages/ts-html-plugin/test/children.test.ts
@@ -13,15 +13,33 @@ it('Ensure PropsWithChildren works as normal', async () => {
     export function Test({ children }: Extension) {
       return <div>{children}</div>;
     }
+    
+    export function Test2(props: Extension) {
+      return <div>{props.children}</div>;
+    }
+    
+    export function Test3({ children }: PropsWithChildren) {
+      return <div>{children}</div>;
+    }
+    
+    export function Test4(props: PropsWithChildren) {
+      return <div>{props.children}</div>;
+    }
+    
+    export function Test5(props?: PropsWithChildren) {
+      return <div>{props?.children}</div>;
+    }
 
     const element = <div />;
 
     export default (
       <>
         <div>
-          <Test>
-             {element}
-          </Test>
+          <Test>{element}</Test>
+          <Test2>{element}</Test2>
+          <Test3>{element}</Test3>
+          <Test4>{element}</Test4>
+          <Test5>{element}</Test5>
         </div>
 
         <div>
